7ef2416e54c0ba10dd58b69fa80a25b6dea6bea1
kosala <kosala.me@gmail.com>
2017-02-06 12:09:34 +0530

    added settings example file

--- api/settings.example.php
+++ api/settings.example.php
@@ -5,16 +5,16 @@ $settings = [
         'displayErrorDetails' => false,
 
         'db' => [
-            'host' => 'localhost',
-            'user' => 'root',
-            'password' => 'root',
-            'schema' => 'slimtest',
+            'host' => 'db-host',
+            'user' => 'db-user',
+            'password' => 'db-password',
+            'schema' => 'db-schema',
         ],
 
         'auth' => [
-            'secret' => 'secretkey',
-            'issuer' => '',
-            'subject' => '',
+            'secret' => 'token-secret-key',
+            'issuer' => 'token-issuer',
+            'subject' => 'token-subject',
             'lifetime' => 86400, // in seconds (24 hours)
         ]
     ]
